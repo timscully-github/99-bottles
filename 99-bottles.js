e0ead422e6ebb573675ed8a489810d3159d30bcb
Tim Scully <hello@timscully.co.uk>
2020-05-13 12:38:27 +0100

    easier to create three outputs instead of creating statments to alter each depending on lyrics for zero and 1

--- 99-bottles.js
+++ 99-bottles.js
@@ -1,18 +1,21 @@
 var n = 100;
 var plural;
+var minusOne;
 
 function beer() {
   while(n >= 1){
+
     n--;
-    if (n === 1) {
-      plural = "";
-    } else {
-      plural = "s"
-    }
+
+    // change lyric for zero
     if (n === 0) {
-      n = "no"
+        console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall");
+    } else if (n === 1) {
+        console.log("1 bottle of beer on the wall, 1 bottle of beer. Take 1 down, pass it around, no more bottles of beer on the wall");
+    } else {
+        console.log(n + " bottles of beer on the wall, bottles of beer. Take 1 down, pass it around, " + (n - 1) + " bottles of beer on the wall");
     }
-    console.log(n + " bottle" + plural + " of beer on the wall, " + n + " bottle" + plural + " of beer. Take 1 down, pass it around, " + (n - 1) + " bottle" + plural + " of beer on the wall");
+
   }
 }
 
